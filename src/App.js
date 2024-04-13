3ba2cc43b33a43ee64789d2aabd06e505be9450a
teokartvelishvili <qartvelishviliteona@gmail.com>
2024-04-13 13:00:13 -0800

    pl

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Services from './Pages/Services/Services';
 import About from './Pages/About/About';
 import Contact from './Pages/Contact/Contact';
 
-
 import {  createBrowserRouter,
 createRoutesFromElements,
 Route,
